cfc21baf1164fea633d97e955173f81c889a23d4
anatoleRch <anatoleroche.blq@gmail.com>
2025-03-16 16:19:39 +0100

    nom +prix sur product

--- frontend/src/app/product/product.component.ts
+++ frontend/src/app/product/product.component.ts
@@ -1,16 +1,27 @@
 import { Component, Input } from '@angular/core';
 import { Product } from '../world';
 import { WebserviceService } from '../webservice.service';
+import {MyProgressBarComponent, Orientation} from './progressbar.component'
+import { BigvaluePipe } from "../bigvalue.pipe";
 
 @Component({
   selector: 'app-product',
-  imports: [],
+  imports: [MyProgressBarComponent, BigvaluePipe],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
+BuyProduct() {
+}
+startFabrication() {
+}
   protected server: string;
   product: Product = new Product();
+  orientation: Orientation = Orientation.horizontal;
+  vitesse: number = 0;
+  initialValue: number =0;
+  run: boolean = false;
+  auto: boolean = false;
   constructor(private service: WebserviceService) {
     this.server = service.server + '/'
   }
@@ -19,7 +30,10 @@ export class ProductComponent {
   set prod(value: Product) {
  
     this.product = value;
-    console.dir(value)
+    this.vitesse = this.product.vitesse
+    this.initialValue = 0
+    this.run = false
+    this.auto = this.product.managerUnlocked
   }
 }
 

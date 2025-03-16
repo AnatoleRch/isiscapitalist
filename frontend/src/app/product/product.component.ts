399574de7a735b58e0ec1d78291ac3cb9edf0d81
anatoleRch <anatoleroche.blq@gmail.com>
2025-03-16 20:00:04 +0100

    on essaye product

--- frontend/src/app/product/product.component.ts
+++ frontend/src/app/product/product.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { Product } from '../world';
+import { Palier, Product } from '../world';
 import { WebserviceService } from '../webservice.service';
 import { MyProgressBarComponent, Orientation } from './progressbar.component'
 import { BigvaluePipe } from "../bigvalue.pipe";
@@ -11,10 +11,7 @@ import { BigvaluePipe } from "../bigvalue.pipe";
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
-  BuyProduct() {
-  }
-  startFabrication() {
-  }
+  
   protected server: string;
   product: Product = new Product();
   orientation: Orientation = Orientation.horizontal;
@@ -35,5 +32,44 @@ export class ProductComponent {
     this.run = false
     this.auto = this.product.managerUnlocked
   }
+
+  progressbarvalue: number = 0
+  setProgress(value: number) {
+    if (value >= 0 && value <= 100) {
+      this.progressbarvalue = value;
+    } else if (value < 0) {
+      this.progressbarvalue = 0;
+    } else {
+      this.progressbarvalue = 100;
+    }
+  }
+  sleep(ms :number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  startProduction() {
+    this.product.timeleft = this.product.vitesse
+    this.run = true
+    this.sleep(this.product.vitesse).then(() => { this.run = false });
+  }
+  
+  checkPalier(p: Product) {
+    let nb = p.paliers.length;
+    for (let i = 0; i < nb; i = i + 1) {
+      if (p.quantite >= p.paliers[i].seuil && p.paliers[i].unlocked == false) {
+        p.paliers[i].unlocked = true;
+        this.updateGainVitesse(this.product, this.product.paliers[i])
+      }
+    }
+  }
+
+  updateGainVitesse(produit :Product, palier: Palier) {
+    // tous les palliers
+    if (palier.typeratio === "vitesse") {
+      produit.vitesse = produit.vitesse / palier.ratio
+    }
+    if (palier.typeratio === "gain") {
+      produit.revenu = produit.revenu * palier.ratio
+    }
+}
 }
 
